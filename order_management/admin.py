f732f0038fb0be930ace737702440e007cd51167
smdsu <109036946+smdsu@users.noreply.github.com>
2025-02-11 22:15:07 +0300

    Update admin.py

--- order_management/admin.py
+++ order_management/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Order, OrderItems
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'table_number', 'status', 'created_at']
+    list_filter = ['status', 'created_at']
+    date_hierarchy = 'created_at'
+    ordering = ['created_at']
+    show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(OrderItems)
+class OrderItemsAdmin(admin.ModelAdmin):
+    list_display = ['order', 'name', 'price', 'quantity']
+    list_filter = ['order']
+    search_fields = ['name']
+    raw_id_fields = ['order']
+    show_facets = admin.ShowFacets.ALWAYS
