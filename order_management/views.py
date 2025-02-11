76d1f2e22264302b1bf811a05c012d22a982096c
smdsu <109036946+smdsu@users.noreply.github.com>
2025-02-11 22:14:37 +0300

    Update views.py
    Add OrderListView based on generic ListView

--- order_management/views.py
+++ order_management/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Order
 
-# Create your views here.
+
+class OrderListView(ListView):
+    queryset = Order.objects.all()
+    context_object_name = 'orders'
+    paginate_by = 10
+    template_name = 'order_management/order/list.html'
