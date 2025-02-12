4536c1b30dcc54bd0091fca760cc0edbf6faeaaf
smdsu <109036946+smdsu@users.noreply.github.com>
2025-02-12 20:25:53 +0300

    ManyToMany models
    Redesigned models, implemented many-to-many linkage for the Order model to the Item model
    Corresponding changes in admin panel

--- order_management/admin.py
+++ order_management/admin.py
@@ -1,19 +1,25 @@
 from django.contrib import admin
-from .models import Order, OrderItems
+from .models import Order, Item, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'table_number', 'status', 'created_at']
+    list_display = ['id', 'table_number', 'status', 'total_price', 'created_at']
     list_filter = ['status', 'created_at']
     date_hierarchy = 'created_at'
-    ordering = ['created_at']
+    ordering = ['-created_at']
+    fields = ('table_number', 'status')
+    inlines = [OrderItemInline,]
     show_facets = admin.ShowFacets.ALWAYS
 
-@admin.register(OrderItems)
-class OrderItemsAdmin(admin.ModelAdmin):
-    list_display = ['order', 'name', 'price', 'quantity']
-    list_filter = ['order']
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
     search_fields = ['name']
-    raw_id_fields = ['order']
-    show_facets = admin.ShowFacets.ALWAYS
+
+
--- order_management/models.py
+++ order_management/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.db.models import Sum, F
 
 
+class Item(models.Model):
+    name = models.CharField(max_length=256, verbose_name="Name of dish")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
+
+    def __str__(self):
+        return self.name
+
+
 class Order(models.Model):
 
     class Status(models.TextChoices):
@@ -23,6 +31,7 @@ class Order(models.Model):
         verbose_name="Status",
     )
 
+    items = models.ManyToManyField(Item, through='OrderItem')
     total_price = models.DecimalField(
         max_digits=10,
         decimal_places=2,
@@ -35,34 +44,46 @@ class Order(models.Model):
 
     def update_total_price(self):
         """Recalculates the total order value and saves it."""
-        total = self.items.aggregate(total=Sum(F("price") * F("quantity")))["total"] or 0
+        if self.status in ["RE", "PA"]:
+            return # Если заказ завершен - перерасчет не выполняется
+        
+        total = self.orderitem_set.aggregate(
+            total=Sum(F("item__price") * F("quantity"))
+        )["total"] or 0
         self.total_price = total
         self.save()
     
     def __str__(self):
-        return f"Order {self.id}, (Table {self.table_number})"
+        return f"Order {self.id} — Table {self.table_number}"
 
 
-class OrderItems(models.Model):
+class OrderItem(models.Model):
     order = models.ForeignKey(
         Order,
         on_delete=models.CASCADE,
-        related_name="items"
     )
-    name = models.CharField(max_length=256, verbose_name="Name of dish")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
+    item = models.ForeignKey(
+        Item,
+        on_delete=models.CASCADE,
+    )
     quantity = models.PositiveIntegerField(default=1, verbose_name="Quantity")
 
     def save(self, *args, **kwargs):
         """Updates the order total price when a dish is added/changed."""
-        super().save(*args, **kwargs)
-        self.order.update_total_price()
+        if self.order.status not in ["RE", "PA"]:
+            super().save(*args, **kwargs)
+            self.order.update_total_price()
+        else:
+            raise ValueError("You cannot modify an order with a status of 'Ready' or 'Paid'.")
     
     def delete(self, *args, **kwargs):
         """Updates the order total price when a dish is deleted."""
-        super().delete(*args, **kwargs)
-        self.order.update_total_price()
+        if self.order.status not in ["RE", "PA"]:
+            super().delete(*args, **kwargs)
+            self.order.update_total_price()
+        else:
+            raise ValueError("You cannot modify an order with a status of 'Ready' or 'Paid'.")
     
     def __str__(self):
-        return f"{self.name} x {self.quantity}"
+        return f"{self.quantity} x {self.item.price} for order {self.order.id}"
 
