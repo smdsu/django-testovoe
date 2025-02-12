a3462ac7b3f25ab5b0468b6bd082e83f0ce2754c
smdsu <109036946+smdsu@users.noreply.github.com>
2025-02-12 21:09:22 +0300

    Update urls.py

--- order_management/urls.py
+++ order_management/urls.py
@@ -7,5 +7,6 @@ app_name = 'orders'
 
 urlpatterns = [
     path('', views.OrderListView.as_view(), name='order_list'),
-    path('<int:pk>/', views.OrderDetailView.as_view(), name='order_detail')
+    path('<int:pk>/', views.OrderDetailView.as_view(), name='order_detail'),
+    path('add/', views.create_order, name='order_add'),
 ]
