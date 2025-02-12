03089fb5fbdf189c27afb3a491db5891c81d98cf
smdsu <109036946+smdsu@users.noreply.github.com>
2025-02-12 21:09:19 +0300

    create order view

--- order_management/views.py
+++ order_management/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
+
 from .models import Order
+from .forms import OrderAddForm, OrderItemFormSet
 
 
 class OrderListView(ListView):
@@ -12,3 +14,27 @@ class OrderListView(ListView):
 class OrderDetailView(DetailView):
     model = Order
     template_name = 'order_management/order/detail.html'
+
+
+def create_order(request):
+    if request.method == 'POST':
+        form = OrderAddForm(request.POST)
+        formset = OrderItemFormSet(request.POST)
+
+        if form.is_valid() and formset.is_valid():
+            order = form.save()
+
+            formset.instance = order
+            formset.save()
+
+            return redirect(order.get_abs_url())
+    else:
+        form = OrderAddForm()
+        formset = OrderItemFormSet()
+
+    context = {
+        'form': form,
+        'formset': formset,
+    }
+
+    return render(request, 'order_management/order/add.html', context)
