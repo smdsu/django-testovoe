bed64b12d03c969f1e94850f776df3abdd358a01
smdsu <109036946+smdsu@users.noreply.github.com>
2025-02-13 18:52:51 +0300

    Change list view and add status filters

--- order_management/urls.py
+++ order_management/urls.py
@@ -6,7 +6,8 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('', views.OrderListView.as_view(), name='order_list'),
+    path('', views.orders_list, name='order_list'),
+    path('<str:status>/', views.orders_list, name='order_status_list'),
     path('<int:pk>/', views.OrderDetailView.as_view(), name='order_detail'),
     path('add/', views.create_order, name='order_add'),
 ]
--- order_management/views.py
+++ order_management/views.py
@@ -1,15 +1,10 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 
 from .models import Order
 from .forms import OrderAddForm, OrderItemFormSet
 
-
-class OrderListView(ListView):
-    queryset = Order.objects.all()
-    context_object_name = 'orders'
-    paginate_by = 10
-    template_name = 'order_management/order/list.html'
 
 class OrderDetailView(DetailView):
     model = Order
@@ -38,3 +33,26 @@ def create_order(request):
     }
 
     return render(request, 'order_management/order/add.html', context)
+
+
+def orders_list(request, status: str = None):
+    if status:
+        orders_list = Order.objects.filter(status=status)
+    else:
+        orders_list = Order.objects.all()
+    
+    paginator = Paginator(orders_list, 5)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        orders = paginator.page(page_number)
+    except PageNotAnInteger:
+        orders = paginator.page(1)
+    except EmptyPage:
+        orders = paginator.page(paginator.num_pages)
+
+    context = {
+        'orders': orders,
+    }
+
+    return render(request, 'order_management/order/list.html', context)
