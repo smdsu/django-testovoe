002089eb77f07f29b779b3bae475f13f17af6677
smdsu <109036946+smdsu@users.noreply.github.com>
2025-02-15 17:10:38 +0300

    Create cafe_staff group

--- order_management/apps.py
+++ order_management/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
 
-
 class OrderManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'order_management'
+
+
+    def ready(self):
+        from django.db.models.signals import post_migrate
+        from django.contrib.auth.models import Group
+
+        def create_groups(sender, **kwargs):
+            Group.objects.get_or_create(name="cafe_staff")
+        
+        post_migrate.connect(create_groups, sender=self)
+        
